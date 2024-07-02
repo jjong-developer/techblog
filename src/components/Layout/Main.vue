18e9ecfb80f9bc7e1613b0d1f6f0dc1e5eee22fe
jongwook2 <72546383+jongwook2@users.noreply.github.com>
2024-07-02 23:51:54 +0900

    폴더 구조 변경 진행중

---
+++ src/components/Layout/Main.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <div>메인 페이지</div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia'; /* 기존의 상태 관리 라이브러리 Vuex를 대체할 수 있는 라이브러리 */
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import '@/assets/scss/base/reset.scss'; /* 초기화 세팅 */
+import '@/assets/scss/common/common.scss'; /* 공통 레이아웃 */
+import '@/assets/scss/common/header.scss'; /* 상단 */
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.mount('#app');
