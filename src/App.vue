c35f8dbdfbae10f16b71b5220db6e56857457ab9
jongwook2 <72546383+jongwook2@users.noreply.github.com>
2024-07-25 22:57:37 +0900

    scss 정리

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
 
 <script lang="ts" setup>
 import { RouterLink, RouterView } from 'vue-router'
-import Header from '@/components/Common/Header.vue';
-import Footer from '@/components/Common/Footer.vue';
+import Header from '@/views/common/Header.vue';
+import Footer from '@/views/common/Footer.vue';
 </script>
 
 <style lang="scss" scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia'; /* 기존의 상태 관리 라이브러리 Vuex를 대체할 수 있는 라이브러리 */
+import { createPinia } from 'pinia'; // 기존의 상태 관리 라이브러리 Vuex를 대체할 수 있는 라이브러리
 import App from './App.vue';
 import router from './router';
 
-/* 폰트 설정 */
+/* 폰트 */
 import '@/assets/scss/base/font.scss';
 
-/* 초기화 설정 */
+/* 초기화 */
 import '@/assets/scss/base/reset.scss';
 
 /* 공통 레이아웃 */
@@ -15,7 +15,7 @@ import '@/assets/scss/common/header.scss'; // 헤더
 import '@/assets/scss/common/footer.scss'; // 푸터
 
 /* 뷰 페이지 */
-import '@/assets/scss/views/main.scss'; // 메인 페이지
+import '@/assets/scss/main.scss'; // 메인 페이지
 import '@/assets/scss/views/list.scss'; // 리스트 페이지
 import '@/assets/scss/views/view.scss'; // 뷰어 페이지
 
